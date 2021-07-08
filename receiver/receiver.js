3293ab5648945b0d9d0ef36cd68009818dca4881
Rinki <kririnki146@gmail.com>
2021-07-07 21:36:40 -0700

    first commit

---
+++ receiver/receiver.js
@@ -0,0 +1,101 @@
+/*
+const webSocket = new WebSocket("ws://192.168.214.166:3000")
+webSocket.onmessage = (Event)=> {
+    handlesignallingdata(JSON.parse(Event.data))
+}
+function handlesignallingdata(data)
+{
+    switch(data.type)
+    { case "offer":
+       peerconnection.setLocationDescription(data.offer)
+       createAndSendAnswer()
+       break
+     case "candidate":
+         peerconnection.addIceCandidate(data.candidate)
+
+    }
+}
+
+function createAndSendAnswer()
+{
+  peerconnection.createAnswer((answer)=> {
+      peerconnection.setLocationDescription(answer)
+      sendData({
+          type: "send_answer",
+          answer: "answer"
+      },(error)=>{
+          console.log(error)
+      })
+  })
+}
+let peerconnection
+
+function sendData(data) {
+    data.username=username
+    webSocket.send(JSON.stringify(data));
+    
+}
+let peerconnection
+let localStream
+let username
+function JoinCall() {
+    username =  document.getElementById("username-input").value
+    document.getElementById("Video-Call-div").style.display="inline"
+    navigator.getUserMedia({
+        video:
+         {
+            frameRate:24,
+            aspectRatio:1.333333,
+        },
+        audio:true,
+    },(stream)=>{
+        localStream=stream
+        document.getElementById("Local-Video").srcObject = localStream
+        let Configuration = {
+            iceserver: [ 
+                {
+                "url": ["stun4.l.google.com:19302","stun.ekiga.net","stun.voipbuster.com","stun.voipbuster.com"]
+                }
+            ]
+
+        }
+
+       peerconnection= new RTCPeerConnection(Configuration)
+       peerconnection.addStream(localStream)
+       peerconnection.onaddstreme = (e) => {
+
+        document.getElementById("Remote-Video").srcObject=e.stream
+
+       }
+
+       peerconnection.onicecandidate= ((e)=> {
+           if(e.candidate==null)
+           return 
+           sendData({
+               type: "send_candidate",
+               candidate: e.candidate
+           })
+       })
+        
+       sendData({
+           type: "send_candidate",
+           candidate: e.candidate
+       })
+
+    }, (error)=>{
+        console.log(error);
+    })
+    
+}
+ 
+ let IsAudio = true
+ function MuteAudio(IsAudio) {
+     IsAudio=!IsAudio
+
+     localStream.getAudioTracks()[0].enabled =IsAudio
+ }
+ let IsCamera =true
+ function MuteVideo(IsCamera){
+     IsCamera =!IsCamera
+     localStream.getVideoTracks()[0].enabled= IsCamera
+ }*/
---
+++ server.js
@@ -0,0 +1,108 @@
+/*
+const Socket = require("websocket").server
+const http= require("http")
+
+const server = http.createServer((req,res)=> {})
+
+server.listen(3000, () =>{
+    console.log("Listening on port 2000...")
+})
+
+const webSocket = new Socket({httpServer: server})
+
+//array to store data of sender to send reciever when they join the call
+let user= []
+
+
+webSocket.on('request',(req)=>{
+    const connection = req.accept()
+
+    connection.on('message', (mes)=>{
+
+        const data = JSON.parse(message.utf8Data)
+        const user =findUser(username)
+
+        switch(data.type)
+        {
+            case "store_user":
+                if(user!=null)
+                {
+                return 
+                }
+                const newuser={
+                    conn: connection,
+                    username: data.username
+                }
+                user.push(newuser)
+                console.log(username)
+                break
+            case "store_offer" :
+                if(user==null)
+                return
+                user.offer=data.offer 
+                break
+            case "store_candidate": 
+                 if(user==null)
+                 return
+                 if(user.candidate==null)
+                user.candidate= []
+                user.candidate.push=data.candidate
+                break
+            case "send_answer":
+                if(user==null)
+                return
+                sendData({
+                    type: "answer",
+                    answer: data.answer
+                },user.conn)
+                break
+            case "send_candidate":
+                if(user==null)
+                return
+                sendData({
+                    type: "candidate",
+                    candidate: data.candidate
+                },user.conn)
+                break
+            case "join_call":
+                if(user==null)
+                return
+                sendData({
+                    type: "offer",
+                    offer: data.offer
+                },connection)
+
+                user.candidate.forEach(candidate => {
+                    sendData({
+                        type: "candidate",
+                        candidate: user.candidate
+                    },connection)
+
+                })
+                break
+
+        }
+    })
+    //to delete the username from user array after closing connect
+    connection.on('close',(reason,description)=> {
+        user.forEach(user=>{
+            if(user.conn=connection)
+            {
+            user.splice(user.indexOf(user),1)
+            return
+            }
+        })
+    })
+
+})
+
+function sendData(data,conn) {
+    conn.send(JSON.stringify(data))
+    
+}
+function findUser(username) {
+    for(let i=0;i<user.length;i++)
+    if(user[i]==username)
+    return user[i]
+    
+} */
